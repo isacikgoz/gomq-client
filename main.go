af5376a62cd24a767b2589997b3e6c7b042553a1
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-07-02 23:53:51 +0300

    improve on connection handling

--- main.go
+++ main.go
@@ -10,20 +10,39 @@ import (
 	"github.com/isacikgoz/gomq/api"
 )
 
+type BareMessage struct {
+	Message string
+}
+
 func main() {
-	conn, err := net.Dial("udp", "127.0.0.1:12345")
+
+	conn, err := net.Dial(os.Args[1], "127.0.0.1:12345")
 	defer conn.Close()
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
 	}
+	sub := &api.AnnotatedMessage{
+		Command: "SUB",
+		Target:  "topic_1",
+	}
+
+	b, err := json.Marshal(sub)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	conn.Write(b)
+
+	go listenAndPrint(conn)
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		text := s.Text()
 		if text == "exit" {
 			return
 		}
-		pl := api.BareMessage{
+		pl := BareMessage{
 			Message: s.Text(),
 		}
 		plData, err := json.Marshal(pl)
@@ -32,15 +51,27 @@ func main() {
 			os.Exit(1)
 		}
 		msg := &api.AnnotatedMessage{
-			Command: "pub",
+			Command: "PUB",
 			Target:  "topic_1",
 			Payload: plData,
 		}
-		b, err := json.Marshal(msg)
+		b, err = json.Marshal(msg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "error wrapping message: %v", err)
 			os.Exit(1)
 		}
 		conn.Write(b)
+	}
+}
+
+func listenAndPrint(conn net.Conn) {
+	for {
+		p := make([]byte, 16384)
+		_, err := bufio.NewReader(conn).Read(p)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "something happened very bad: %v", err)
+			break
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", p)
 	}
 }
