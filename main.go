3a8b6833a4e406511f672bba0e79adc6a3376ef4
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-07-05 00:01:00 +0300

    revoew code make it more understandable

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/isacikgoz/gomq/api"
 )
@@ -18,27 +20,47 @@ type BareMessage struct {
 
 func main() {
 
+	var inc chan *api.AnnotatedMessage
+	var out chan *api.AnnotatedMessage
+
 	conn, err := net.Dial(os.Args[1], "127.0.0.1:12345")
 	defer conn.Close()
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
 	}
-	sub := &api.AnnotatedMessage{
-		Command: "SUB",
-		Target:  "topic_1",
-	}
+	subscribe(conn)
 
-	b, err := json.Marshal(sub)
-	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
-	}
-	conn.Write(b)
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, syscall.SIGTERM, os.Interrupt)
 
-	go listenAndPrint(conn)
+	go func() {
+		for {
+			select {
+			case msg := <-inc:
+				var bare BareMessage
+				json.Unmarshal(msg.Payload, &bare)
+				log.Printf("%s\n", bare.Message)
+			case msg := <-out:
+				b, err := json.Marshal(msg)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error wrapping message: %v", err)
+					os.Exit(1)
+				}
+				conn.Write(b)
+			}
+		}
+	}()
 
+	go listenAndPrint(conn, inc)
+	go listenInput(conn, out)
+
+	<-interrupt
+}
+
+func listenInput(conn net.Conn, ch chan<- *api.AnnotatedMessage) {
 	s := bufio.NewScanner(os.Stdin)
+
 	for s.Scan() {
 		text := s.Text()
 		if text == "exit" {
@@ -57,16 +79,11 @@ func main() {
 			Target:  "topic_1",
 			Payload: plData,
 		}
-		b, err = json.Marshal(msg)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error wrapping message: %v", err)
-			os.Exit(1)
-		}
-		conn.Write(b)
+		ch <- msg
 	}
 }
 
-func listenAndPrint(conn net.Conn) {
+func listenAndPrint(conn net.Conn, ch chan<- *api.AnnotatedMessage) {
 	for {
 		p := make([]byte, 16384)
 		n, err := bufio.NewReader(conn).Read(p)
@@ -80,8 +97,20 @@ func listenAndPrint(conn net.Conn) {
 			fmt.Fprintf(os.Stdout, "something happened: %v\n", err)
 			fmt.Fprintf(os.Stdout, "message is: %s\n", p)
 		}
-		var bare BareMessage
-		json.Unmarshal(msg.Payload, &bare)
-		log.Printf("%s\n", bare.Message)
+		ch <- &msg
 	}
+}
+
+func subscribe(conn net.Conn) {
+	sub := &api.AnnotatedMessage{
+		Command: "SUB",
+		Target:  "topic_1",
+	}
+
+	b, err := json.Marshal(sub)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	conn.Write(b)
 }
